ded62d18a31076ae27d2eba44046779974047bac
Mike <101659911+snorlaxbubba@users.noreply.github.com>
2025-01-22 21:04:04 -0800

    added models and controllers

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,29 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\{
+    CardController,
+    CategoryController,
+    ProductController,
+    RarityController,
+    SetController,
+    SeriesController,
+    UserController,
+    UserCollectionController,
+    UserWishlistController
+};
 
 // Grouping routes with a prefix (e.g., api/v1/)
 Route::prefix('public')->group(function () {
+    Route::get('/users', [UserController::class, 'index']);
+});
+
+// Grouping routes with a prefix (e.g., api/v1/)
+Route::prefix('private')->group(function () {
+    Route::get('/users', [UserController::class, 'index']);
+});
+
+// Grouping routes with a prefix (e.g., api/v1/)
+Route::prefix('api')->group(function () {
     Route::get('/users', [UserController::class, 'index']);
 });
