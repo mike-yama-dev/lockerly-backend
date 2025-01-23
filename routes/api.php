f4f7ae570420da067d73b7e546967026f7c68047
Mike <101659911+snorlaxbubba@users.noreply.github.com>
2025-01-22 17:50:26 -0800

    added api routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Grouping routes with a prefix (e.g., api/v1/)
+Route::prefix('v1')->group(function () {
+    Route::get('/users', [UserController::class, 'index']);
+});
