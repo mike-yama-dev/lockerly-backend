3103d00f9c9a09c7c749e716ae9efb10a2f29504
Mike <101659911+snorlaxbubba@users.noreply.github.com>
2025-01-22 17:53:09 -0800

    added api public routes

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\UserController;
 
 // Grouping routes with a prefix (e.g., api/v1/)
-Route::prefix('v1')->group(function () {
+Route::prefix('public')->group(function () {
     Route::get('/users', [UserController::class, 'index']);
 });
